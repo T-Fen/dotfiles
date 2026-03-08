52f5c239fc2ab69d33ad0da2a483488ea7097de9
Trey Sizemore <hjsizemore@gmail.com>
2026-03-08 13:58:55 -0400

    Enable calendar module in init.el, clean up packages.el

--- doom/packages.el
+++ doom/packages.el
@@ -1,9 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;; (package! some-package)
 (package! org-gcal)
 (package! calfw)
 (package! calfw-org)
